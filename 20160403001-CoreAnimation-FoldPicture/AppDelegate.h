d233e09a77340546678235cf21a26096ca325cf9
raineryue <it_jinlei.yue@163.com>
2016-04-03 23:54:32 +0800

    Initial Commit

---
+++ 20160403001-CoreAnimation-FoldPicture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160403001-CoreAnimation-FoldPicture
+//
+//  Created by Rainer on 16/4/3.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20160403001-CoreAnimation-FoldPicture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160403001-CoreAnimation-FoldPicture
+//
+//  Created by Rainer on 16/4/3.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
